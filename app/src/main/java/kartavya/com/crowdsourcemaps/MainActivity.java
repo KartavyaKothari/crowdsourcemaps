0911a0a7e68a33cadaf2ce7fbe346b995e758128
Kartavya kothari <kartavya.kothari24@gmail.com>
2018-02-14 13:13:27 +0530

    Linked add place activity

--- app/src/main/java/kartavya/com/crowdsourcemaps/MainActivity.java
+++ app/src/main/java/kartavya/com/crowdsourcemaps/MainActivity.java
@@ -1,13 +1,21 @@
 package kartavya.com.crowdsourcemaps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void addPlace(View view){
+        Intent intent = new Intent(this,AddPlace.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_place_details);
+        setContentView(R.layout.activity_main);
     }
 }
