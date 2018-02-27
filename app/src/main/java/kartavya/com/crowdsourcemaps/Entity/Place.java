e944fe618efb7f038f2e7fa2365fa62170edd659
Vihang Garud <garudvihang0@gmail.com>
2018-02-27 15:27:22 +0530

    Added Place. Changed colors.

---
+++ app/src/main/java/kartavya/com/crowdsourcemaps/Entity/Place.java
@@ -0,0 +1,52 @@
+package kartavya.com.crowdsourcemaps.Entity;
+
+/**
+ * Created by HP on 2/27/2018.
+ */
+
+public class Place {
+
+    private String PlaceID;
+    private String PlaceName;
+    private String PlaceCoordinates;
+    private String PlaceRating;
+
+    public Place(String placeID, String placeName, String placeCoordinates, String placeRating) {
+        PlaceID = placeID;
+        PlaceName = placeName;
+        PlaceCoordinates = placeCoordinates;
+        PlaceRating = placeRating;
+    }
+
+    public String getPlaceID() {
+        return PlaceID;
+    }
+
+    public void setPlaceID(String placeID) {
+        PlaceID = placeID;
+    }
+
+    public String getPlaceName() {
+        return PlaceName;
+    }
+
+    public void setPlaceName(String placeName) {
+        PlaceName = placeName;
+    }
+
+    public String getPlaceCoordinates() {
+        return PlaceCoordinates;
+    }
+
+    public void setPlaceCoordinates(String placeCoordinates) {
+        PlaceCoordinates = placeCoordinates;
+    }
+
+    public String getPlaceRating() {
+        return PlaceRating;
+    }
+
+    public void setPlaceRating(String placeRating) {
+        PlaceRating = placeRating;
+    }
+}
